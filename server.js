21c2cdf72e8120e810bb8f7ed239d1b1e35a9d74
Shivam Singh <Shivam.singxh+git@gmail.com>
2025-05-09 15:59:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(, () => {
-  console.log('Server running on http://localhost:');
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
